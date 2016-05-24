b7532e6838cd42a49687fd50c50c88bfb669f4e7
wml14 <wml14@software.nju.edu.cn>
2016-05-24 20:11:33 +0800

    asd

---
+++ AnyQuant3/src/main/java/bl/impl/NewAnalysis.java
@@ -0,0 +1,18 @@
+package bl.impl;
+
+import bl.analyse.TechnicalAnalysisStrategy;
+
+/**
+ * Created by kylin on 16/5/24.
+ * All rights reserved.
+ */
+public class NewAnalysis {
+
+    private TechnicalAnalysisStrategy analysisStrategy;
+
+    public String analyseRSI(String number){
+
+        return "";
+    }
+
+}
